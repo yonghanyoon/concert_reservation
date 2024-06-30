9392ffa4371779e69cd7eaf3eb53478b05bf8ed3
윤용한 <88192173+yonghanyoon@users.noreply.github.com>
2024-06-30 23:02:01 +0900

    Add files via upload

---
+++ src/main/java/com/hhplus/concert/ConcertApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.concert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertApplication.class, args);
+	}
+
+}
