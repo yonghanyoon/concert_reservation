3ce5c206a1c3ccc6eb1f34148f5c26b67ce2a9c3
yonghanyoon <yoonyh95@naver.com>
2024-08-01 21:16:21 +0900

    [feat] Redis 캐싱 config 및 캐싱처리 추가

--- src/main/java/com/hhplus/concert/api/concert/presentation/ConcertController.java
+++ src/main/java/com/hhplus/concert/api/concert/presentation/ConcertController.java
@@ -7,13 +7,13 @@ import com.hhplus.concert.api.concert.presentation.dto.response.SeatResDTO;
 import com.hhplus.concert.api.concert.presentation.mapper.ConcertMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "ConcertController", description = "예약 가능한 콘서트 정보를 제공하기 위한 API")
@@ -30,8 +30,9 @@ public class ConcertController {
     // 콘서트 조회 API
     @Operation(summary = "콘서트 조회 API")
     @GetMapping("")
-    public ResponseEntity<List<ConcertResDTO>> getConcerts(@RequestHeader String uuid) {
-        return ResponseEntity.ok(ConcertMapper.toDtoFromConcert(concertService.getConcerts()));
+    public ResponseEntity<List<ConcertResDTO>> getConcerts(@RequestHeader String uuid, @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(ConcertMapper.toDtoFromConcert(concertService.getConcerts(page, size)));
     }
 
     // 예약 가능 날짜 조회 API
