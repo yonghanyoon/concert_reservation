44dec8c56dd5920c43ab3fb074f8093940931502
yonghanyoon <yoonyh95@naver.com>
2024-08-15 23:07:44 +0900

    [feat]

--- src/main/java/com/hhplus/concert/api/reservation/domain/repository/ReservationCreatedEventRepository.java
+++ src/main/java/com/hhplus/concert/api/reservation/domain/repository/ReservationCreatedEventRepository.java
@@ -2,9 +2,10 @@ package com.hhplus.concert.api.reservation.domain.repository;
 
 import com.hhplus.concert.api.reservation.domain.entity.ReservationCreatedEvent;
 import com.hhplus.concert.api.reservation.domain.type.ReservationEventStatus;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationCreatedEventRepository extends JpaRepository<ReservationCreatedEvent, Long> {
-    List<ReservationCreatedEvent> findAllByReservationEventStatus(ReservationEventStatus reservationEventStatus);
+    List<ReservationCreatedEvent> findAllByReservationEventStatusAndCreatedAtBefore(ReservationEventStatus reservationEventStatus, LocalDateTime createdAt);
 }
